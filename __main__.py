cfa9bfca55abfaabb1d66e5cfcd4c0b35568383a
Mike Pagel <mike@mpagel.de>
2024-06-27 18:08:02 +0200

    package installation with Pulumi sudo

--- __main__.py
+++ __main__.py
@@ -11,7 +11,16 @@ connection = pulumi_command.remote.ConnectionArgs(
     private_key=config.require_secret('ssh-private-key'),
 )
 
-ls = pulumi_command.remote.Command('ls', connection=connection, create='ls -la')
+build_system = pulumi_command.remote.Command(
+    'python build system',
+    connection=connection,
+    create='sudo apt update -y && sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev '
+    'libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils '
+    'tk-dev libffi-dev liblzma-dev python3-openssl git',
+)
 
-pulumi.export('ls stdout', ls.stdout)
-pulumi.export('ls stderr', ls.stderr)
+pulumi.export('build_system stdout', build_system.stdout)
+pulumi.export('build_system stderr', build_system.stderr)
+
+# TODO: build a component to install Python3.12 on PVE following:
+#  https://wiki.crowncloud.net/?How_to_Install_Python_3_12_on_Debian_12
