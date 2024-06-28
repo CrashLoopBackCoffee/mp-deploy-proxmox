5f48c48ccb96cf21bc6e916b9f1875a924f064bf
Mike Pagel <mike@mpagel.de>
2024-06-28 17:47:31 +0200

    refactor Python installer into component

--- __main__.py
+++ __main__.py
@@ -3,6 +3,9 @@
 import pulumi
 import pulumi_command
 
+import pve
+import pve.python
+
 config = pulumi.Config()
 
 connection = pulumi_command.remote.ConnectionArgs(
@@ -11,67 +14,11 @@ connection = pulumi_command.remote.ConnectionArgs(
     private_key=config.require_secret('ssh-private-key'),
 )
 
-packages = (
-    'build-essential',
-    'libssl-dev',
-    'zlib1g-dev',
-    'libbz2-dev',
-    'libreadline-dev',
-    'libsqlite3-dev',
-    'wget',
-    'curl',
-    'llvm',
-    'libncurses5-dev',
-    'libncursesw5-dev',
-    'xz-utils',
-    'tk-dev',
-    'libffi-dev',
-    'liblzma-dev',
-    'python3-openssl',
-    'git',
-)
-build_system = pulumi_command.remote.Command(
-    'python build system',
+version = config.require('python-version')
+python = pve.python.Python(
+    f'python-{version}',
     connection=connection,
-    create=' && '.join(
-        (
-            'apt-get update -y',
-            f'apt-get install --no-install-recommends -y {' '.join(packages)}',
-        )
-    ),
+    version=version,
 )
 
-python_version = config.require('python-version')
-python_sources = pulumi_command.remote.Command(
-    'python sources',
-    connection=connection,
-    create=' && '.join(
-        (
-            f'wget https://www.python.org/ftp/python/{python_version}/Python-{python_version}.tgz',
-            f'tar -xf Python-{python_version}.tgz && rm -f Python-{python_version}.tgz',
-        )
-    ),
-    delete='rm -rf Python-{python_version}',
-    opts=pulumi.ResourceOptions(depends_on=[build_system]),
-)
-
-python_build = pulumi_command.remote.Command(
-    'python build',
-    connection=connection,
-    create=' && '.join(
-        (
-            f'cd Python-{python_version}',
-            './configure --enable-optimizations',
-            'make -j 2',
-        )
-    ),
-    delete='make clean -c Python-{python_version}',
-    opts=pulumi.ResourceOptions(depends_on=[python_sources]),
-)
-
-python_install = pulumi_command.remote.Command(
-    'python install',
-    connection=connection,
-    create=' && '.join((f'cd Python-{python_version}', 'make altinstall')),
-    opts=pulumi.ResourceOptions(depends_on=[python_build]),
-)
+pulumi.export('python-interpreter', python.interpreter_name)
---
+++ pve/python.py
@@ -0,0 +1,102 @@
+"""Pulumi resources to install an up-to-date Python interpreter."""
+
+import pulumi
+import pulumi_command
+
+
+class Python(pulumi.ComponentResource):
+    version = pulumi.Output[str]
+    interpreter_name = pulumi.Output[str]
+
+    build_system_packages = ' '.join(
+        (
+            'build-essential',
+            'libssl-dev',
+            'zlib1g-dev',
+            'libbz2-dev',
+            'libreadline-dev',
+            'libsqlite3-dev',
+            'wget',
+            'curl',
+            'llvm',
+            'libncurses5-dev',
+            'libncursesw5-dev',
+            'xz-utils',
+            'tk-dev',
+            'libffi-dev',
+            'liblzma-dev',
+            'python3-openssl',
+            'git',
+        )
+    )
+
+    def __init__(
+        self,
+        name: str,
+        connection: pulumi.Input[pulumi_command.remote.ConnectionArgs],
+        version: pulumi.Input[str],
+        opts: pulumi.ResourceOptions | None = None,
+    ) -> None:
+        module_path = self.__module__.replace('.', ':')
+        super().__init__(
+            f'{module_path}:{self.__class__}',
+            name,
+            {
+                'version': version,
+                'interpreter_name': None,
+            },
+            opts,
+        )
+
+        build_system = pulumi_command.remote.Command(
+            f'{name}-build-system',
+            connection=connection,
+            create=' && '.join(
+                (
+                    'apt-get update -y',
+                    f'apt-get install --no-install-recommends -y {self.build_system_packages}',
+                )
+            ),
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+
+        python_sources = pulumi_command.remote.Command(
+            f'{name}-sources',
+            connection=connection,
+            create=' && '.join(
+                (
+                    f'wget https://www.python.org/ftp/python/{version}/Python-{version}.tgz',
+                    f'tar -xf Python-{version}.tgz && rm -f Python-{version}.tgz',
+                )
+            ),
+            delete='rm -rf Python-{version}',
+            opts=pulumi.ResourceOptions(depends_on=[build_system], parent=self),
+        )
+
+        python_build = pulumi_command.remote.Command(
+            f'{name}-build',
+            connection=connection,
+            create=' && '.join(
+                (
+                    f'cd Python-{version}',
+                    './configure --enable-optimizations',
+                    'make -j 2',
+                )
+            ),
+            delete='make clean -c Python-{version}',
+            opts=pulumi.ResourceOptions(depends_on=[python_sources], parent=self),
+        )
+
+        pulumi_command.remote.Command(
+            f'{name}-install',
+            connection=connection,
+            create=' && '.join((f'cd Python-{version}', 'make altinstall')),
+            opts=pulumi.ResourceOptions(depends_on=[python_build], parent=self),
+        )
+
+        self.register_outputs(
+            {
+                'version': version,
+                'interpreter_name': pulumi.Output.concat('python', version),
+            }
+        )
